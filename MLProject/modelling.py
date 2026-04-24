498d237a941a13391851eae78c12a22cf8b3f835
danugans <danugans909@gmail.com>
2026-04-24 17:40:11 +0700

    update model final

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,12 +5,18 @@ import mlflow
 import mlflow.sklearn
 import numpy as np
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 warnings.filterwarnings("ignore")
 
-DATA_DIR = "namadataset_preprocessing"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "namadataset_preprocessing")
+MLRUNS_DIR = os.path.join(BASE_DIR, "mlruns")
+
+mlflow.set_tracking_uri(f"file:{MLRUNS_DIR}")
+mlflow.set_experiment("Workflow_CI_Model_Danu-setiawan")
+
 
 def load_data():
     X_train = joblib.load(os.path.join(DATA_DIR, "X_train.joblib"))
@@ -23,29 +29,43 @@ def load_data():
 def train():
     X_train, X_test, y_train, y_test = load_data()
 
-    model = RandomForestClassifier(
-        n_estimators=200,
-        max_depth=15,
-        class_weight="balanced",
-        random_state=42
-    )
+    with mlflow.start_run():
 
-    model.fit(X_train, y_train)
+        model = LogisticRegression(
+            class_weight='balanced',
+            max_iter=1000,
+            random_state=42
+        )
 
-    y_prob = model.predict_proba(X_test)[:, 1]
-    y_pred = (y_prob > 0.3).astype(int)
+        model.fit(X_train, y_train)
 
-    acc = accuracy_score(y_test, y_pred)
-    prec = precision_score(y_test, y_pred)
-    rec = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+        y_prob = model.predict_proba(X_test)[:, 1]
+        threshold = 0.3
+        y_pred = (y_prob > threshold).astype(int)
 
-    mlflow.log_metric("accuracy", acc)
-    mlflow.log_metric("precision", prec)
-    mlflow.log_metric("recall", rec)
-    mlflow.log_metric("f1_score", f1)
+        acc = accuracy_score(y_test, y_pred)
+        prec = precision_score(y_test, y_pred)
+        rec = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
 
-    mlflow.sklearn.log_model(model, "model")
+        print("Accuracy :", round(acc, 4))
+        print("Precision:", round(prec, 4))
+        print("Recall   :", round(rec, 4))
+        print("F1 Score :", round(f1, 4))
+
+        mlflow.log_param("model", "LogisticRegression")
+        mlflow.log_param("threshold", threshold)
+
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("precision", prec)
+        mlflow.log_metric("recall", rec)
+        mlflow.log_metric("f1_score", f1)
+
+        mlflow.sklearn.log_model(
+            model,
+            "model",
+            input_example=X_train[:5]
+        )
 
 
 if __name__ == "__main__":
